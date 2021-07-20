cbf684c23de7d0031458ed87e3857954e123273f
neoruien <neoruien123@gmail.com>
2021-07-20 18:23:50 +0800

    Refactor code

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -4,26 +4,26 @@ import AuthContext from "../../store/auth-context";
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
-  const context = useContext(AuthContext);
+  const ctx = useContext(AuthContext);
   return (
     // <AuthContext.Consumer>
     // {(context) => {
     // return (
     <nav className={classes.nav}>
       <ul>
-        {context.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {context.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {context.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
-            <button onClick={context.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+// default context
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {}, // add dummy function for better IDE autocompletion
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    // Context.Provider can wrap around other components, which will then have access to the Context
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
